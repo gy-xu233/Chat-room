29655f3681fbfa6c0ed1f5bedfad4c81d33f7a47
xugy5 <744315804@qq.com>
2021-02-05 17:14:40 +0800

    chatRoom

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public NetManager netmanager;
+    public InputField input;             // 注意这儿不能直接设置Text并将input field中的Text拖入，会造成text无法修改，傻逼unity
+    void Start()
+    {
+    }
+    public void MySendMessage()
+    {
+        if (input.text != "")
+        {
+            netmanager.MySendMessage(input.text);
+            input.text = "";
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NetManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine.UI;
+
+public class NetManager : MonoBehaviour
+{
+    
+    private Socket customerSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+    private IPAddress clientIP = IPAddress.Parse(""); //输入服务器端IP
+    private int port = 7788;
+    private Thread t;
+    private byte[] data = new byte[1024];
+    private string message = "";
+    public Text ChatCotent;
+    public ScrollRect chatpanel;
+    private int flag = 0;
+    private void ConnectClient()
+    {
+
+    }
+    private void Update()
+    {
+        if (message != "")
+        {
+            ChatCotent.text += message;
+            message = "";
+            flag = 5;
+        }
+        if(flag-- > 0) chatpanel.verticalNormalizedPosition = 0;
+    }
+    void Start()
+    {
+        customerSocket.Connect(new IPEndPoint(clientIP, port));
+        t = new Thread(MyReceiveMessage);
+        t.Start();
+    }
+
+    private void MyReceiveMessage()
+    {
+        while(!customerSocket.Poll(10,SelectMode.SelectRead))
+        {
+            int length = customerSocket.Receive(data);
+            message = Encoding.UTF8.GetString(data,0,length) + "\n";
+        }
+    }
+
+    public void MySendMessage(string message)
+    {
+        customerSocket.Send(Encoding.UTF8.GetBytes(message));
+        Debug.Log(message);
+    }
+    private void OnDestroy()
+    {
+        t.Abort();
+        customerSocket.Shutdown(SocketShutdown.Both);
+        customerSocket.Close();
+    }
+}
